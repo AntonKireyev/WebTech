690eceebacefd01c0af900c6546bbdc2cbbf2565
AntonKireyev <akireyev1@babson.edu>
2023-04-14 10:55:45 -0400

    ex 23 and game

---
+++ game/game.js
@@ -0,0 +1,22 @@
+// define things
+button = document.getElementById('start');
+shape = document.getElementById('shape');
+// arrays for shape and color
+shapeBorder = ['0', '50']
+color = ['red', 'green', 'blue', 'yellow', 'violet']
+// make the function to set x and y, shape, color, and show the shape.
+
+function getShape(){
+    let start = new Date().getTime()
+    // let x = 300
+    // let y = 300
+    
+    // shape.style.display = 'block';
+    // shape.style.borderRadius = shapeBorder[];
+    // shape.style.color = color[];
+}
+
+function calcTime(){
+    let end = new Date().getTime()
+    
+}
