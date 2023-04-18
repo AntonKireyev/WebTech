dc4c7dec5525165eb8ded7ad40e5a6904c4752e1
AntonKireyev <akireyev1@babson.edu>
2023-04-18 19:46:04 -0400

    4/18 class updates

--- game/game.js
+++ game/game.js
@@ -1,22 +1,71 @@
 // define things
-button = document.getElementById('start');
-shape = document.getElementById('shape');
+const button = document.getElementById("start");
+const shape = document.getElementById("shape");
+let startTime;
 // arrays for shape and color
-shapeBorder = ['0', '50']
-color = ['red', 'green', 'blue', 'yellow', 'violet']
+// const shapeBorder = ["0", "50"];
+// const color = ["red", "green", "blue", "yellow", "violet"];
 // make the function to set x and y, shape, color, and show the shape.
 
-function getShape(){
-    let start = new Date().getTime()
-    // let x = 300
-    // let y = 300
-    
-    // shape.style.display = 'block';
-    // shape.style.borderRadius = shapeBorder[];
-    // shape.style.color = color[];
+function getShape() {
+  //   // random integer
+  //   const randIndex = Math.floor(Math.random() * 2);
+  //   // let x = 300
+  //   // let y = 300
+  //   shape.style.borderRadius = shapeBorder[randIndex];
+  //   // shape.style.color = color[];
+
+  // set random shape
+  if (Math.random > 0.5) {
+    shape.style.borderRadius = "50%";
+  } else {
+    shape.style.borderRadius = "0";
+  }
+
+  // set random size, height in range [50, 150]
+  const randomLength = Math.random() * 100 + 50;
+  shape.style.width = randomLength + "px";
+  shape.style.height = randomLength = "px";
+
+  // set random position
+  const top = Math.random() * (window.innerHeight - 150 - 150);
+  const left = Math.random() * (window.innerWidth - 150);
+  shape.style.top = top + "px";
+  shape.style.left = left + "px";
+
+  // set random color
+  shape.style.color = createRandomColor();
+  // display shape
+  shape.style.display = "block";
+}
+function createRandomColor() {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  return `${red}, ${green}, ${blue}`;
+}
+function startGame() {
+  console.log("... starting the game");
+  getShape();
+  startTime = new Date().getTime();
 }
 
-function calcTime(){
-    let end = new Date().getTime()
-    
+function displayReactionTime() {
+  let endTime = new Date().getTime();
+  let duration = (endTime - startTime) / 1000;
+  document.getElementById("timeTaken").textContent = duration + "s";
 }
+
+function endRound() {
+  shape.style.display = "none";
+  displayReactionTime();
+  setTimeout(startGame, 100);
+}
+
+button.addEventListener("click", function () {
+  startGame();
+});
+
+shape.addEventListener("click", function () {
+  endRound();
+});
